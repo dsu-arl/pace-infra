870eb5d7c40c8073ac335fb126397bff9c5091de
Mason Schmidgall <mason.schmidgall@madren.org>
2025-05-22 13:31:41 -0500

    Get user correctly in logout_override

--- dojo_plugin/pages/auth.py
+++ dojo_plugin/pages/auth.py
@@ -1,12 +1,12 @@
-from CTFd.utils import user as current_user
+from CTFd.utils import user
 from CTFd.auth import logout
 from ..api.v1.docker import remove_container
 
 def logout_override():
-    if current_user.authed():
+    if user.authed():
         # Logout must continue even if this fails
         try:
-            remove_container(current_user)
+            remove_container(user.get_current_user())
         except Exception:
             pass
     return logout()
