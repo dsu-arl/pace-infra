f74ba01adbb4d63ac6b732b81d19ef8781f1728a
Mason Schmidgall <mason.schmidgall@madren.org>
2025-05-27 14:12:28 -0500

    Update container stoppage on logout to respect preferences

--- dojo_plugin/pages/auth.py
+++ dojo_plugin/pages/auth.py
@@ -1,12 +1,17 @@
-from CTFd.utils import user
+from CTFd.utils.user import authed, get_current_user
 from CTFd.auth import logout
 from ..api.v1.docker import remove_container
+from ..models import UserChallengePreferences
 
 def logout_override():
-    if user.authed():
+    if authed():
         # Logout must continue even if this fails
         try:
-            remove_container(user.get_current_user())
+            user = get_current_user()
+            chall_prefs = UserChallengePreferences.query.filter_by(user_id=user.id).first()
+            # If no preference, default to stopping container
+            if chall_prefs is None or chall_prefs.stop_on_logout:
+                remove_container(user)
         except Exception:
             pass
     return logout()
